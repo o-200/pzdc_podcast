22aca3cc753b004606420617fbf46c99be34106b
krillan <Pushkinidze49@gmail.com>
2025-03-08 19:13:48 +0300

    updated. spec configs updated

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,8 +8,9 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 # that will avoid rails generators crashing because migrations haven't been run yet
 # return unless Rails.env.test?
 require 'rspec/rails'
-require 'support/capybara'
-require 'support/session_helper'
+require "capybara/rspec"
+require "selenium/webdriver"
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 
 # Add additional requires below this line. Rails is not loaded until this point!
 
